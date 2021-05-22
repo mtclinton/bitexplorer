8be2d4f36ddb9b4b510bc472372dc874b143881f
mtclinton <maxtclinton@gmail.com>
2021-05-22 17:35:22 -0400

    show latest block data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,43 @@ import {API_URL} from '../constants'
 function Home() {
 
     const [data, setData] = useState([])
+    const [block, setBlock] = useState([])
+
 
     // ***** fetch data *****
     const fetchData = async url => {
+
         try {
-            const response = await fetch(url)
-            const res = await response.json()
-            setData(res.data)
+
+            // Call the API
+            fetch(url).then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return Promise.reject(response);
+                }
+            }).then(function (data) {
+
+                // Store the post data to a variable
+                let btc_data = data.data;
+                setData(btc_data)
+
+                // Fetch another API
+                return fetch(`${API_URL}get_blockhash/BTC/${btc_data.blocks}`);
+
+            }).then(function (response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return Promise.reject(response);
+                }
+            }).then(function (block_data) {
+                setBlock(block_data.data)
+
+            }).catch(function (error) {
+                console.warn(error);
+            });
+
         } catch (err) {
             console.log("Error:", err)
         }
@@ -19,6 +49,8 @@ function Home() {
 
     useEffect(() => {
         fetchData(  `${API_URL}get_info/BTC`)
+
+        fetchData()
     }, [])
 
     return (
@@ -28,7 +60,18 @@ function Home() {
               <div className="crypto-item-container" key={i}>
                   <span className="crypto-item">{keyName} : {data[keyName]}</span>
               </div>
+
           ))}
+          <br />
+          <div>
+              <h1>Block: {data.blocks}</h1>
+              {Object.keys(block).map((keyName, i) => (
+                  <div className="crypto-item-container" key={i}>
+                      <span className="crypto-item">{keyName} : {block[keyName]}</span>
+                  </div>
+
+              ))}
+          </div>
       </div>
     );
 }
